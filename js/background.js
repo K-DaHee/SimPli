1b759e4f09d6890cd0b8cdda096cd256b505733d
K-DaHee <dhk086512@gmail.com>
2026-04-01 23:25:10 +0900

    feat: 확장프로그램 기본 환경 세팅

---
+++ js/background.js
@@ -0,0 +1,3 @@
+console.log('🎵 SimPli: Background Service Worker Loaded!');
+
+// 추후 Offscreen API를 활용한 백그라운드 오디오 통신이 구현될 예정입니다.
---
+++ js/popup.js
@@ -0,0 +1,3 @@
+console.log('🎵 SimPli: Popup Script Loaded!');
+
+// UI 로직이 추가될 예정입니다.
